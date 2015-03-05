a39a9c6bdea50348f7624f4f603f803d825321db
Sauloh <saulo.hachem@gmail.com>
2015-03-05 15:11:22 -0500

    read ips from list and bing_key from environment variable

--- yaet.py
+++ yaet.py
@@ -2,17 +2,19 @@
 #-*- coding:utf-8 -*-
 
 from bingpy import WebSearch
-import sys 
+import sys, os
 
+# Initializing data
+bing_key = os.environ.get('BING_KEY') # Get BING_API from Environment Variable
 output = open(sys.argv[3], 'a') # Output File
 inputIps = open(sys.argv[1], 'r') # Input Ips
 nResults = int(sys.argv[2])	# Number of Results
 
-web = WebSearch("") # BING API
-pages = web.search("IP:", nResults)
+web = WebSearch(bing_key) # BING API
 
-
-for page in pages:
-	output.write("%s - %s\n" % (page.title.encode('utf-8'), page.url.encode('utf-8')))
+for ip in inputIps.readlines():
+	pages = web.search("IP:%s" % (ip), nResults)
+	for page in pages:
+		output.write("%s - %s\n" % (page.title.encode('utf-8'), page.url.encode('utf-8')))
 
 output.close()
